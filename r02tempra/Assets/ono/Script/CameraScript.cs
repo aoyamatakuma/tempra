e9a2906acc2451d3f175ccdf97bc7e54abfb40b6
KPC-017A1750\K017A1750 <uta17kome50@gmail.com>
2020-02-06 15:46:00 +0900

    cameraScript,cameraScript2の追加（ono）

---
+++ r02tempra/Assets/ono/Script/CameraScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    Transform cameraTrans;
+    [SerializeField] Transform playerTrans;
+    [SerializeField] Vector3 cameraVec; 
+    [SerializeField] Vector3 cameraRot; 
+    void Awake()
+    {
+        cameraTrans = transform;
+        cameraTrans.rotation = Quaternion.Euler(cameraRot);
+    }
+    void LateUpdate()
+    {
+        cameraTrans.position = Vector3.Lerp(cameraTrans.position, playerTrans.position + cameraVec, 2.0f * Time.deltaTime);
+    }
+}
