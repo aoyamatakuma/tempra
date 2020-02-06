ad55d8263651b272f612b1524b22b72244917cc6
VANO0125 <43366986+VANO0125@users.noreply.github.com>
2020-02-06 16:07:26 +0900

    tagにAburaを追加してください Playerの挙動と状態変化 ステージはtileMapでやってます

---
+++ r02tempra/Assets/KIKUCHI/Script/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed =0.1f;
+    SpriteRenderer sprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+
+    void Move()
+    {
+        Vector2 position = transform.position;
+        if (Input.GetKey(KeyCode.D))
+        {
+            position.x += moveSpeed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            position.x -= moveSpeed;
+        }
+        transform.position = position;
+    }
+    void ModeChange()
+    {
+        sprite.color = new Color(0, 0, 0, 1);
+    }
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Abura"))
+        {
+            ModeChange();
+        }
+    } 
+}
