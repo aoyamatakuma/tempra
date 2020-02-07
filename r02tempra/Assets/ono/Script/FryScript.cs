bf7edbf8631dabc55a4bf71e0f9d05e74f719a66
KPC-017A1750\K017A1750 <uta17kome50@gmail.com>
2020-02-07 15:30:18 +0900

    浮いてる感じの追加（ono）

---
+++ r02tempra/Assets/ono/Script/FryScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FryScript : MonoBehaviour
+{
+
+    float angle = 0;
+    public float range = 1f;//幅
+    float yspeed = 0.04f;//はやさ
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector2 sin = transform.position;
+        sin.y += Mathf.Sin(angle) * range;
+        angle += yspeed;
+        transform.position = sin;
+      
+    }
+}
