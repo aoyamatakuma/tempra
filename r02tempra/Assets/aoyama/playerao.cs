9d0c934c30ffd15b21bf96219a7771d51209a94c
KPC-017A1400\ren <yama04210421@gmail.com>
2020-02-07 10:25:57 +0900

    いったん

---
+++ r02tempra/Assets/aoyama/playerao.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerao : MonoBehaviour
+
+{
+
+    Rigidbody2D rigidPlayer;//物理演算
+    private float jumpForce = 250.0f;//ジャンプの力
+    private float speed = 2.0f;//地上での移動速度
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void Move()//移動系
+    {
+        float h = Input.GetAxis("Horizontal");
+        rigidPlayer.velocity = new Vector2(speed * h, rigidPlayer.velocity.y);
+    }
+}
