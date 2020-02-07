4b108281ec0e860b89f9024a3548c76cfcbc2383
VANO0125 <43366986+VANO0125@users.noreply.github.com>
2020-02-07 09:31:22 +0900

    移動処理の変更とジャンプ処理(仮)

--- r02tempra/Assets/KIKUCHI/Script/Player.cs
+++ r02tempra/Assets/KIKUCHI/Script/Player.cs
@@ -5,28 +5,40 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField]
-    private float moveSpeed =0.1f;
+    private float moveSpeed =0.5f;
+    [SerializeField]
+    private float jumpSpeed = 1000f;
+    bool jumpFlag;
     SpriteRenderer sprite;
+    Rigidbody2D rig;
     // Start is called before the first frame update
     void Start()
     {
         sprite = gameObject.GetComponent<SpriteRenderer>();
+        rig = gameObject.GetComponent<Rigidbody2D>();
+        jumpFlag = false;
     }
 
 
     void Move()
     {
+        float hor = Input.GetAxis("Horizontal");
         Vector2 position = transform.position;
-        if (Input.GetKey(KeyCode.D))
+        if (hor != 0)
         {
-            position.x += moveSpeed;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            position.x -= moveSpeed;
+            position.x += moveSpeed * hor;
         }
         transform.position = position;
     }
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && jumpFlag == false)
+        {
+            rig.AddForce(Vector2.up * jumpSpeed);
+            jumpFlag = true;
+        }
+    }
+
     void ModeChange()
     {
         sprite.color = new Color(0, 0, 0, 1);
@@ -37,6 +49,7 @@ public class Player : MonoBehaviour
     void Update()
     {
         Move();
+        Jump();
     }
 
     void OnTriggerEnter2D(Collider2D col)
@@ -45,5 +58,15 @@ public class Player : MonoBehaviour
         {
             ModeChange();
         }
+
+        
     } 
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Stage"))
+        {
+            jumpFlag = false;
+        }
+    }
 }
