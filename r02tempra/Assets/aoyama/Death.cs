f77854c15fb7469949226ff18cc3dbbd91b97f77
KPC-017A1400\ren <yama04210421@gmail.com>
2020-03-25 17:00:35 +0900

    破裂追加

---
+++ r02tempra/Assets/aoyama/Death.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Death : MonoBehaviour
+{
+    [SerializeField]
+        private GameObject explosionEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            Explosion();
+        }
+    }
+
+    public void Explosion()
+    {
+        Instantiate(explosionEffect, transform.position, Quaternion.identity);
+        Destroy(this.gameObject);       
+    }
+}
