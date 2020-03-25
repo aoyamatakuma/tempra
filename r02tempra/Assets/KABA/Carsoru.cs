c9721ddd3f63c2bb226458848919b0a0ae9f890c
KPC-017A1393\K017A1393 <kabaya.k017a1393@gmail.com>
2020-03-25 14:17:12 +0900

    カーソルをステージ(仮オブジェクト)に重ねてたら色が変わるなどの反応追加(なんかバグったらごめんね)

---
+++ r02tempra/Assets/KABA/Carsoru.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Carsoru : MonoBehaviour
+{
+    SpriteRenderer sprite;
+    // カーソルが対象オブジェクトに入った時
+    void Start()
+    {
+       
+        sprite = gameObject.GetComponent<SpriteRenderer>();
+
+    }
+    void OnMouseEnter()
+    {
+        Debug.Log("OnMouseEnter");
+        sprite.color = new Color(1, 0, 0, 1);
+    }
+
+    // カーソルが対象オブジェクトから出た時
+    void OnMouseExit()
+    {
+        Debug.Log("OnMouseEnterexit");
+        sprite.color = new Color(255, 255, 255, 1);
+    }
+}
