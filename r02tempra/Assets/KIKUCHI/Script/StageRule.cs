c6cbcf84a66334dcfeb77c58b680278201c68805
VANO0125 <43366986+VANO0125@users.noreply.github.com>
2020-03-25 15:44:09 +0900

    泡の数によってステージが浮く（泡の生成をステージの子オブジェクトに変更後動作確認）

---
+++ r02tempra/Assets/KIKUCHI/Script/StageRule.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageRule : MonoBehaviour
+{
+    [SerializeField]
+    private playerao player;
+    //ステージが浮く時の泡の数
+    [SerializeField]
+    private int limit_bubble =3;
+    //現在の泡の数
+    [SerializeField]
+    public int current_bubble;
+
+    List<Transform> allChildren = new List<Transform>();
+    // Start is called before the first frame update
+    void Start()
+
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BubbleCount();
+        FlyRule();
+    }
+
+    //ステージ内のbubbleタグのついた子オブジェクトの数取得
+    void BubbleCount()
+    {
+        if (!player.awaCreate) return;
+        Transform chiledTransform = transform.GetComponentInChildren<Transform>();
+        foreach(var child in chiledTransform)
+        {
+            allChildren.Add(chiledTransform);
+        }
+        for(int i = 0; i <= allChildren.Count; i++)
+        {
+            if(allChildren[i].tag == "bubble")
+            {
+                current_bubble++;
+            }
+        }
+
+        player.awaCreate = false;
+    }
+
+    //浮く
+    void FlyRule()
+    {
+       if(limit_bubble <= current_bubble)
+        {
+            Debug.Log("浮いたあああああああああああああああああ");
+        }
+
+    }
+}
