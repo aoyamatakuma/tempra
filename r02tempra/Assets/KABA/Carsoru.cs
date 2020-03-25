2e11b1f3e53acf5508c32d5a55ae73d0e5ee95ed
KPC-017A1393\K017A1393 <kabaya.k017a1393@gmail.com>
2020-03-25 18:15:15 +0900

    修正(ゴリ押し)、個人的にあまりいい出来ではないので統合するかは任せます。

--- r02tempra/Assets/KABA/Carsoru.cs
+++ r02tempra/Assets/KABA/Carsoru.cs
@@ -5,23 +5,30 @@ using UnityEngine;
 public class Carsoru : MonoBehaviour
 {
     SpriteRenderer sprite;
+    [SerializeField]
+    public CameraControl CameraCO;
+  
     // カーソルが対象オブジェクトに入った時
     void Start()
     {
-       
+
         sprite = gameObject.GetComponent<SpriteRenderer>();
-
+    
     }
-    void OnMouseEnter()
+    void Update()
     {
-        Debug.Log("OnMouseEnter");
-        sprite.color = new Color(1, 0, 0, 1);
-    }
-
-    // カーソルが対象オブジェクトから出た時
-    void OnMouseExit()
-    {
-        Debug.Log("OnMouseEnterexit");
-        sprite.color = new Color(255, 255, 255, 1);
+        if (Input.GetButtonDown("Y_BUTTON") && CameraCO.isCameraPos2)
+        {
+            Debug.Log("触れてる");
+            sprite.color = new Color(1, 0, 0, 1);
+        }
+        if (Input.GetButtonDown("Y_BUTTON") && CameraCO.isCameraPos2 == false)
+        {
+            Debug.Log("触れてないよ");
+            sprite.color = new Color(255, 255, 255, 1);
+        }
+     
     }
 }
+    // カーソルが対象オブジェクトから出た時
+    
