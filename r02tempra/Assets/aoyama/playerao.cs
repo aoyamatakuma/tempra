06d17fe5c4c71768bee4cca6ca356cc380477d67
vano0125 <kikuchi.k017a1642@gmail.com>
2020-04-09 18:18:39 +0900

    コライダーの範囲変更

--- r02tempra/Assets/aoyama/playerao.cs
+++ r02tempra/Assets/aoyama/playerao.cs
@@ -78,7 +78,7 @@ public class playerao : MonoBehaviour
             ModeChange();
         }
 
-        if (col.gameObject.CompareTag("Border"))
+        if (col.gameObject.CompareTag("StageArea"))
         {
             stageParent = col.gameObject.transform.root;
             transform.parent = col.gameObject.transform.root;
