1577da6e12aa364334859b42ed388442ed5f4a8c
vano0125 <kikuchi.k017a1642@gmail.com>
2020-04-16 18:08:33 +0900

    泡の数のtext

---
+++ r02tempra/Assets/KIKUCHI/Script/BubbleText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BubbleText : MonoBehaviour
+{
+    StageRule stage;
+    Text bubbleText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bubbleText = gameObject.GetComponent<Text>();
+        stage = transform.root.gameObject.GetComponent<StageRule>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bubbleText.text = stage.current_bubble + "/" + stage.limit_bubble;
+    }
+}
