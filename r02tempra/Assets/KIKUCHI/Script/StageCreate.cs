5006eef87288628d95f2eb05bea34e4736b36ebc
KPC-017A1393\K017A1393 <kabaya.k017a1393@gmail.com>
2020-04-28 16:06:58 +0900

    とりあえず、現状をプッシュ

--- r02tempra/Assets/KIKUCHI/Script/StageCreate.cs
+++ r02tempra/Assets/KIKUCHI/Script/StageCreate.cs
@@ -8,38 +8,41 @@ public class StageCreate : MonoBehaviour
     public StageData stageData;
     private  int width;//盤上の行
     private  int height;//盤上の列
-    private int StageNumbers;
+    private int StageNumbers;//ステージの番号
     [SerializeField] 
     private float magnification = 36;
     private int[,]posStage;
     Dictionary<int, GameObject> stageObject = new Dictionary<int, GameObject>();
 
-
-    void Awake()
+    //Start
+    //Awake
+    //OnEnable
+    void Start()
     {
 
         width  = stageData.Xmax;
         height = stageData.Ymax;
-      
-          posStage = new int[3,3];
+        StageNumbers = stageData.Number_S;
+        posStage = new int[height,width];
+       
         for(int i = 0; i < height; i++)
         {
             for(int j = 0; j < width; j++)
             {
                 posStage[i, j] = 0;
             }
-        }
-        for(int i = 0; i < stageData.StageObjList.Count; i++)
+        } 
+        for (int i = 0; i < stageData.StageObjList.Count; i++)
         {
             stageObject.Add(i + 1, stageData.StageObjList[i]);
         }
-       
         //ステージ管理
         Arrrayswitch();
-        Debug.Log("活動中");
       
-
+        Debug.Log("活動中");
         Create();
+
+        
     }
     void Update()
     {
@@ -57,14 +60,16 @@ public class StageCreate : MonoBehaviour
                 {
                     Instantiate(stageObject[posStage[i, j]]);
                     stageObject[posStage[i, j]].transform.position = new Vector3(j * magnification, i*magnification, 0);
-
                 }
             }
         }
     }
+    void ArraryNum(int x, int y, int value)
+    {
+        posStage[y, x] = value;
+    }
     void Arrrayswitch()
     {
-        StageNumbers = stageData.Number_S;
         //ステージ管理
         switch (StageNumbers)
         {
@@ -83,15 +88,13 @@ public class StageCreate : MonoBehaviour
                 Debug.Log("１の場合");
                 //break文
                 break;
+            case 2:
+                ArraryNum(2, 2, 2);
+                ArraryNum(0, 2, 1);
+                ArraryNum(0, 0, 1);
+                Debug.Log("2の場合");
+                //break文
+                break;
         }
-
     }
-    void ArraryNum(int x,int y,int value)
-    {
-        posStage[y, x] = value;
-    }
-    
-
-
-
 }
