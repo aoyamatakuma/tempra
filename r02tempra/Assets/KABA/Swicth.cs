212f0647b04ffdd72fc50e7539282340d7691287
KPC-017A1393\K017A1393 <kabaya.k017a1393@gmail.com>
2020-05-25 09:24:19 +0900

    念のためswitchのスクリプト

---
+++ r02tempra/Assets/KABA/Swicth.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swicth : MonoBehaviour
+{
+    public GameObject obj;
+    //移動状態を表すフラグ
+
+
+    void Start()
+    {
+        obj = GameObject.FindGameObjectWithTag("");
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(obj.gameObject);
+        }
+
+    }
+}
