79c1d78c9ccc18dbbd54ce0f1f76f0d402e02608
小山 <raw25251001@gmail.com>
2020-06-03 08:34:48 +0900

    スクリプトの更新 青山ステージ番号変えたからスクリプトの変更よろしく

--- r02tempra/Assets/koyama/Script/ClearButton.cs
+++ r02tempra/Assets/koyama/Script/ClearButton.cs
@@ -6,11 +6,16 @@ using UnityEngine.UI;
 
 public class ClearButton : MonoBehaviour
 {
-    
+
 
     [SerializeField]
     //Scene スクリプト取得
     private SceneM gameManager;
+    [SerializeField]
+    //フェードイン処理
+    private GameObject fadeInPrefab;
+    private GameObject fadeInInstance;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,28 +25,31 @@ public class ClearButton : MonoBehaviour
         //読み込む
         gameManager = GameObject.Find("GameManager").GetComponent<SceneM>();
         gameManager = gameManager.GetComponent<SceneM>();
+        //フェードイン消去
+        Destroy(fadeInInstance);
     }
 
     // Update is called once per frame
     void Update()
     {
-      
+
     }
     public void OnClick()
     {
         //名前を取得して分岐
         switch (transform.name)
         {
-            case "Button":
-                NextSecene();
+            case "Button": 
+                //コルーチン開始
+                StartCoroutine("NextStageCoroutine");
                 break;
             case "Button1":
-                //Debug.Log ("ステージセレクト");
-                SceneManager.LoadScene("select");
+                //コルーチン開始
+                StartCoroutine("StageSelectCoroutine");
                 break;
             case "Button2":
-                //Debug.Log ("タイトル");
-                SceneManager.LoadScene("MasterTitle");
+                //コルーチン開始
+                StartCoroutine("MainCoroutine");
                 break;
             default:
                 break;
@@ -59,5 +67,49 @@ public class ClearButton : MonoBehaviour
         {
             SceneManager.LoadScene("stage" + gameManager.currentStageNum);
         }
+    }
+    public IEnumerator NextStageCoroutine()
+    {
+
+        if (fadeInInstance == null)
+        {
+            yield return new WaitForSeconds(1f);
+            //フェードイン処理
+            fadeInInstance = GameObject.Instantiate(fadeInPrefab) as GameObject;
+            yield return new WaitForSeconds(1f);
+            NextSecene();
+
+        }
+
+    }
+    public IEnumerator StageSelectCoroutine()
+    {
+
+        if (fadeInInstance == null)
+        {
+            yield return new WaitForSeconds(1f);
+            //フェードイン処理
+            fadeInInstance = GameObject.Instantiate(fadeInPrefab) as GameObject;
+            yield return new WaitForSeconds(1f);
+            //Debug.Log ("ステージセレクト");
+            SceneManager.LoadScene("select");
+
+        }
+
+    }
+    public IEnumerator MainCoroutine()
+    {
+
+        if (fadeInInstance == null)
+        {
+            yield return new WaitForSeconds(1f);
+            //フェードイン処理
+            fadeInInstance = GameObject.Instantiate(fadeInPrefab) as GameObject;
+            yield return new WaitForSeconds(1f);
+            //Debug.Log ("タイトル");
+            SceneManager.LoadScene("MasterTitle");
+
+        }
+
     }
 }
