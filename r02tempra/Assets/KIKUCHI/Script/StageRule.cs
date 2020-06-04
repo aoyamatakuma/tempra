4f3b876ba031c0af3b935b41526c295f8910421d
vano0125 <kikuchi.k017a1642@gmail.com>
2020-06-04 17:13:14 +0900

    青山うんち！w

--- r02tempra/Assets/KIKUCHI/Script/StageRule.cs
+++ r02tempra/Assets/KIKUCHI/Script/StageRule.cs
@@ -58,6 +58,7 @@ public class StageRule : MonoBehaviour {
     public GameObject light_Left;
     public GameObject light_Right;
     private GameObject wind_target;
+    private GameRule gameRule;
 
 
 
@@ -71,6 +72,7 @@ public class StageRule : MonoBehaviour {
         Bubblehub = new List<GameObject>();
         BubbleCount_Start();
         player = GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerMove> ();
+        gameRule = GameObject.Find("GameManager").GetComponent<GameRule>();
         flyBool = false;
         downBool = false;
         current_bubble = 0;      
@@ -187,6 +189,7 @@ public class StageRule : MonoBehaviour {
         if (limit_bubble <= current_bubble && !isGoal  && limit_touchBubble <= current_touchBubble && currentStageState != StageState.hit_up && currentStageState != StageState.Wind_hit && currentStageState != StageState.hit_bottom_up) {     
             flyBool = true;
             downBool = false;
+           
             SetCurrentState(StageState.Fly);
         }
 
@@ -203,22 +206,28 @@ public class StageRule : MonoBehaviour {
     }
 
     void FlyMove (Vector2 nextPos) {
-        if (!flyBool && currentStageState != StageState.Fly )return;
+        if ( currentStageState != StageState.Fly )return;
+        Debug.Log(gameRule.getIsPlay());
+        gameRule.falsePlay();
       //  if (currentStageState == StageState.hit_bottom_up) return;
         transform.position = Vector2.MoveTowards(transform.position, nextPos,Time.deltaTime*  speed);
        // transform.position = Vector2.Lerp (transform.position, nextPos, Time.deltaTime * 1f);     
       if(transform.position.y >= nextPos.y || currentStageState != StageState.Fly)
-        {  
+        {
+           gameRule.truePlay();      
             SetCurrentState(StageState.Normal);
         }
     }
 
     void DownMove (Vector2 nextPos) {
-        if (!downBool && currentStageState != StageState.Down ) return;
-       // if (currentStageState == StageState.hit_bottom_up) return;
+        if ( currentStageState != StageState.Down ) return;
+        Debug.Log(gameRule.getIsPlay());
+        gameRule.falsePlay();
+        // if (currentStageState == StageState.hit_bottom_up) return;
         transform.position = Vector2.MoveTowards(transform.position, nextPos, Time.deltaTime * speed);
         if (transform.position.y <= nextPos.y || currentStageState != StageState.Down)
-        { 
+        {
+            gameRule.truePlay();
             SetCurrentState(StageState.Normal);
         }   
     }
@@ -227,13 +236,15 @@ public class StageRule : MonoBehaviour {
     {
         
         if (currentStageState != StageState.Wind_Left && target.gameObject.GetComponent<StageRule>().currentStageState != StageState.Wind_hit ) return;
+        gameRule.falsePlay();
         target. transform.position = Vector2.MoveTowards(target.transform.position, nextPos, Time.deltaTime * speed);
         if (target.transform.position.x <= nextPos.x || target.gameObject.GetComponent<StageRule>().currentStageState != StageState.Wind_hit)
         {
             target.gameObject.GetComponent<StageRule>().SetPosition_Up();
             target.gameObject.GetComponent<StageRule>().SetPosition_Dawn();
             target.gameObject.GetComponent<StageRule>().SetCurrentState(StageState.Normal);
-          //  Debug.Log(target.gameObject.GetComponent<StageRule>().currentStageState);
+            gameRule.truePlay();
+            //  Debug.Log(target.gameObject.GetComponent<StageRule>().currentStageState);
             SetCurrentState(StageState.Normal);
         }
 
@@ -242,12 +253,14 @@ public class StageRule : MonoBehaviour {
     void RightWindMove(Vector2 nextPos, GameObject target)
     {
         if (currentStageState != StageState.Wind_Right && target.gameObject.GetComponent<StageRule>().currentStageState != StageState.Wind_hit ) return;
+       gameRule.falsePlay();
         target.transform.position = Vector2.MoveTowards(target.transform.position, nextPos, Time.deltaTime * speed);
         if (target.transform.position.x >= nextPos.x || target.gameObject.GetComponent<StageRule>().currentStageState != StageState.Wind_hit)
         {
             target.gameObject.GetComponent<StageRule>().SetPosition_Up();
             target.gameObject.GetComponent<StageRule>().SetPosition_Dawn();
             target.gameObject.GetComponent<StageRule>().SetCurrentState(StageState.Normal);
+           gameRule.truePlay();
             SetCurrentState(StageState.Normal);
         }
     }
@@ -335,11 +348,11 @@ public class StageRule : MonoBehaviour {
             }
             if (col.gameObject.CompareTag("Collsion_up") && currentStageState == StageState.hit_bottom_up)
             {
-                SetCurrentState(StageState.hit_bottom);
+                SetCurrentState(StageState.hit_up);
             }
             if (col.gameObject.CompareTag("Collision_bottom") && currentStageState == StageState.hit_bottom_up)
             {
-                SetCurrentState(StageState.hit_up);
+                SetCurrentState(StageState.hit_bottom);
             }
         }
 
