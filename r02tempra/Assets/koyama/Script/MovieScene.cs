136f8b292c8a71aa76ae6c381df66dde4565e8b1
小山 <raw25251001@gmail.com>
2020-06-12 15:34:21 +0900

    OPPAI

---
+++ r02tempra/Assets/koyama/Script/MovieScene.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class MovieScene : MonoBehaviour
+{
+    private VideoPlayer videoPlayer;
+    [SerializeField]
+    //フェードイン処理
+    private GameObject fadeInPrefab;
+    private GameObject fadeInInstance;
+    [SerializeField]
+    //フェードイン処理
+    private GameObject fadeoutPrefab;
+    private GameObject fadeoutInstance;
+
+    bool Button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //フェードイン消去
+        Destroy(fadeInInstance);
+        StartCoroutine("MovieStartCole");
+        Button = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //アイコンの表示非表示のフラグ設定
+        if (Input.GetButtonDown("A_BUTTON")&&!Button)
+        {
+            StartCoroutine("MovieButtonSceneCole");
+        }
+
+    }
+    public IEnumerator MovieStartCole()
+    {
+
+       
+
+        Debug.Log("動画");
+
+
+
+        //フェードイン処理
+        fadeInInstance = GameObject.Instantiate(fadeInPrefab) as GameObject;
+        yield return new WaitForSeconds(0.8f);
+        videoPlayer = GameObject.Find("VideoPlane").GetComponent<VideoPlayer>();
+        videoPlayer.Play();
+        yield return new WaitForSeconds(10.8f);
+        SceneManager.LoadScene("MasterTitle");
+
+    }
+    public IEnumerator MovieButtonSceneCole()
+    {
+        Button = true;
+        fadeoutInstance = GameObject.Instantiate(fadeoutPrefab) as GameObject;
+        yield return new WaitForSeconds(1f);
+
+        SceneManager.LoadScene("MasterTitle");
+        
+
+    }
+    
+}
